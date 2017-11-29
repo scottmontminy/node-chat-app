e66b753d5c7240cbd9d2ca5d6a652e383952f3b3
Scott Montminy <scott@madeinthestudio.com>
2017-11-28 21:35:51 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    to: 'keith@example.com',
-    text: 'All good homie'
-  })
 });
 
 socket.on('disconnect', function () {
